efd92e0f32f87599ec7d7990203c217389176142
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-09-24 14:35:36 +0300

    Initial commit

---
+++ src/test/kotlin/com/uesar/chirp/ChirpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uesar.chirp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChirpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
