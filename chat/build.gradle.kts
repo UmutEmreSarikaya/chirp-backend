133abfddf33c99a8a1d5621691da9730ab8888d3
UmutEmreSarikaya <u.emresarikaya@gmail.com>
2025-09-25 12:46:52 +0300

    Configure gradle files for multi-module project

--- chat/build.gradle.kts
+++ chat/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.spring)
-    alias(libs.plugins.spring.boot)
-    alias(libs.plugins.spring.dependency.management)
-    alias(libs.plugins.kotlin.jpa)
+    id("java-library")
+    id("chirp.spring-boot-service")
+    kotlin("plugin.jpa")
 }
 
 group = "com.uesar"
@@ -15,6 +13,7 @@ repositories {
 }
 
 dependencies {
+        implementation(projects.common)
     testImplementation(kotlin("test"))
 }
 
